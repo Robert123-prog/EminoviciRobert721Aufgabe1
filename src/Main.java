27bbfe177288a0a75e2cfc18c935f1b313bb3242
OOP <roeminovici@gmail.com>
2025-01-15 13:09:19 +0200

    b)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Reader reader = new Reader("C:\\Proiecte\\Proiecte faculta\\Anul_2\\MAP\\Practic\\EminoviciRobert721Aufgabe1\\src\\evenimente.tsv");
         reader.readFromFile().forEach(ereigniss -> System.out.println(ereigniss.toString()));
+        System.out.println("#########");
+        List<Ereigniss> ereignissList = reader.readFromFile();
+        Operation operation = new Operation(ereignissList);
+        operation.filterByName('J').forEach(System.out::println);
     }
 }
--- src/Operation.java
+++ src/Operation.java
@@ -1,2 +1,16 @@
+import java.util.List;
+
 public class Operation {
+    private List<Ereigniss> ereignissList;
+
+    public Operation(List<Ereigniss> ereignissList) {
+        this.ereignissList = ereignissList;
+    }
+
+    public List<String> filterByName(Character initial){
+        return ereignissList.stream()
+                .filter(ereigniss -> ereigniss.getName().charAt(0) == initial)
+                .map(ereigniss -> ereigniss.getName())
+                .toList();
+    }
 }
